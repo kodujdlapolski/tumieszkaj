1b7d7f7f3958a59c46979c890f542228a744f8ba
Maciej Gawinecki <mgawinecki@gmail.com>
2019-11-12 17:03:22 +0100

    #1 Add new data crawled with new Scrapy based-crawler.

---
+++ gus/data/levels.jl
@@ -0,0 +1,8 @@
+{"id": 0, "name": "Poziom Polski"}
+{"id": 1, "name": "Poziom Makroregion\u00f3w"}
+{"id": 2, "name": "Poziom Wojew\u00f3dztw"}
+{"id": 3, "name": "Poziom Region\u00f3w"}
+{"id": 4, "name": "Poziom Podregion\u00f3w"}
+{"id": 5, "name": "Poziom Powiat\u00f3w"}
+{"id": 6, "name": "Poziom Gmin"}
+{"id": 7, "name": "Poziom miejscowo\u015bci statystycznej"}
