0134e0f13a8d625db56c2daf68d4d35d85cca531
Maciej Gawinecki <mgawinecki@gmail.com>
2020-01-07 16:27:38 +0100

    Fix GUS metadata.

--- data/gus/metadata/attributes.jl
+++ data/gus/metadata/attributes.jl
@@ -15,20 +15,3 @@
 {"id": 95, "name": "- lub 0", "symbol": "n", "description": "Dana jeszcze niedost\u0119pna, b\u0119dzie dost\u0119pna"}
 {"id": 96, "name": "- lub 0", "symbol": "n", "description": "Dana jeszcze niedost\u0119pna, b\u0119dzie dost\u0119pna"}
 {"id": 97, "name": "warto\u015b\u0107", "symbol": "p", "description": "\u0141\u0105cznie dla powiatu i miasta na prawach powiatu"}
-{"id": 0, "name": "0", "symbol": " ", "description": ""}
-{"id": 1, "name": "warto\u015b\u0107", "symbol": " ", "description": ""}
-{"id": 3, "name": "warto\u015b\u0107", "symbol": "k", "description": "Agregat mo\u017ce by\u0107 niekompletny"}
-{"id": 4, "name": "0", "symbol": "x", "description": "Wype\u0142nienie pozycji jest niemo\u017cliwe lub niecelowe (w tym ze wzgl\u0119du na wysoki b\u0142\u0105d losowy pr\u00f3by w badaniach reprezentacyjnych, np. BAEL); brak informacji wiarygodnych lub por\u00f3wnywalnych"}
-{"id": 6, "name": "0", "symbol": "x", "description": "Wype\u0142nienie pozycji jest niemo\u017cliwe lub niecelowe (w tym ze wzgl\u0119du na wysoki b\u0142\u0105d losowy pr\u00f3by w badaniach reprezentacyjnych, np. BAEL); brak informacji wiarygodnych lub por\u00f3wnywalnych"}
-{"id": 7, "name": "0", "symbol": "a", "description": "Warto\u015b\u0107 mniejsza ni\u017c przyj\u0119ty format prezentacji"}
-{"id": 11, "name": "warto\u015b\u0107", "symbol": "m", "description": "Zmiany metodologiczne"}
-{"id": 13, "name": "warto\u015b\u0107", "symbol": "k", "description": "Agregat mo\u017ce by\u0107 niekompletny"}
-{"id": 14, "name": "0", "symbol": "x", "description": "Wype\u0142nienie pozycji jest niemo\u017cliwe lub niecelowe (w tym ze wzgl\u0119du na wysoki b\u0142\u0105d losowy pr\u00f3by w badaniach reprezentacyjnych, np. BAEL); brak informacji wiarygodnych lub por\u00f3wnywalnych"}
-{"id": 15, "name": "-", "symbol": " ", "description": "Znak '-' oznacza brak informacji z powodu: zmiany poziomu prezentacji, zmian wprowadzonych do wykazu jednostek terytorialnych lub modyfikacji listy cech w danym okresie sprawozdawczym"}
-{"id": 16, "name": "0", "symbol": "x", "description": "Wype\u0142nienie pozycji jest niemo\u017cliwe lub niecelowe (w tym ze wzgl\u0119du na wysoki b\u0142\u0105d losowy pr\u00f3by w badaniach reprezentacyjnych, np. BAEL); brak informacji wiarygodnych lub por\u00f3wnywalnych"}
-{"id": 17, "name": "0", "symbol": "a", "description": "Warto\u015b\u0107 mniejsza ni\u017c przyj\u0119ty format prezentacji"}
-{"id": 91, "name": "0", "symbol": "#", "description": "Tajemnica statystyczna"}
-{"id": 94, "name": "0", "symbol": "x", "description": "Wype\u0142nienie pozycji jest niemo\u017cliwe lub niecelowe (w tym ze wzgl\u0119du na wysoki b\u0142\u0105d losowy pr\u00f3by w badaniach reprezentacyjnych, np. BAEL); brak informacji wiarygodnych lub por\u00f3wnywalnych"}
-{"id": 95, "name": "- lub 0", "symbol": "n", "description": "Dana jeszcze niedost\u0119pna, b\u0119dzie dost\u0119pna"}
-{"id": 96, "name": "- lub 0", "symbol": "n", "description": "Dana jeszcze niedost\u0119pna, b\u0119dzie dost\u0119pna"}
-{"id": 97, "name": "warto\u015b\u0107", "symbol": "p", "description": "\u0141\u0105cznie dla powiatu i miasta na prawach powiatu"}
